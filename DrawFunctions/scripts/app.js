aeb9ed3af7dee3df653588d0ca868212a11600d9
Михаил Зырянов <mzyranov26@gmail.com>
2023-11-06 09:46:02 -0800

    DrawFunction(without separation) zoom in/out

--- DrawFunctions/scripts/app.js
+++ DrawFunctions/scripts/app.js
@@ -6,10 +6,16 @@
 //     Add canvas to the whole page and resize it depending on the proportion
 //     Before that release all the data from canvas
 
+//var graph = new Graph()
+
+import GraphRenderer from './logic.js'
+
+
 const app = Vue.createApp({
   // data, functions
   data() {
     return {
+      graph: new GraphRenderer.Graph(),
       xDistance: graph.xDistance,
       yDistance: graph.yDistance,
       x0: graph.x0,  // doesn't work properly
@@ -36,135 +42,32 @@ const app = Vue.createApp({
   },
   methods:{
     drawFunction(){
-      drawFunction(graph.funcAmount)
+      GraphRenderer.drawFunction(graph.funcAmount)
     },
-
+    
     renderFunction(F){
-      renderFunction(F, {})
+      GraphRenderer.renderFunction(F, {})
     },
-
+    
     xCoord(x){
-      return (x - this.xMin) / (this.xMax - this.xMin) * this.canvasWidth
+      GraphRenderer.xCoord(x)
     },
 
     yCoord(y){
-      let yMin = this.xMin * this.canvasHeight / this.canvasWidth
-      let yMax = this.xMax * this.canvasHeight / this.canvasWidth
-      return this.canvasHeight - ((y- yMin) / (yMax - yMin) * this.canvasHeight)
+      GraphRenderer.yCoord(y)
     },
 
     // 
     setupCanvas() {
-      let canvas = document.getElementById("canvas")
-      canvas.style.background = "#ff8"
-    
-      const diff = this.wHeight / this.wWidth
-      canvas.width = this.canvasWidth
-      canvas.height = this.canvasHeight
-      
-      let ctx = canvas.getContext("2d")
-      ctx.clearRect(0, 0, canvas.width, canvas.height)
-
-      let yMin = this.xMin * this.canvasHeight / this.canvasWidth
-      let yMax = this.xMax * this.canvasHeight / this.canvasWidth
-
-      ctx.save()
-      ctx.lineWidth = 2
-
-      // Y axis
-      ctx.beginPath()
-      ctx.moveTo(this.xCoord(this.x0), this.yCoord(this.y0))
-      ctx.lineTo(this.xCoord(this.x0), this.yCoord(yMax))
-      // -Y axis
-      ctx.moveTo(this.xCoord(this.x0), this.yCoord(this.y0))
-      ctx.lineTo(this.xCoord(this.x0), this.yCoord(yMin))
-      ctx.stroke() // drawing
-
-      // Y Divisions
-      ctx.beginPath()
-      for (let i = this.yDistance; i < yMax; i+=this.yDistance){
-        ctx.moveTo(this.xCoord(this.x0) - 4, this.yCoord(i))
-        ctx.lineTo(this.xCoord(this.x0) + 4, this.yCoord(i))
-        ctx.fillText(i.toString(), this.xCoord(this.x0) + 8, this.yCoord(i))
-      }
-      for (let i = -this.yDistance; i > yMin; i-=this.yDistance){
-        ctx.moveTo(this.xCoord(this.x0) - 4, this.yCoord(i))
-        ctx.lineTo(this.xCoord(this.x0) + 4, this.yCoord(i))
-        ctx.fillText(i.toString(), this.xCoord(this.x0) + 8, this.yCoord(i))
-      }
-      ctx.stroke()
-      
-      // Arrows (Y)
-      ctx.beginPath()
-      ctx.moveTo(this.xCoord(this.x0), this.yCoord(yMax))
-      ctx.lineTo(this.xCoord(this.x0 - 0.5), this.yCoord(yMax-0.5))
-      ctx.moveTo(this.xCoord(this.x0), this.yCoord(yMax))
-      ctx.lineTo(this.xCoord(this.x0 + 0.5), this.yCoord(yMax-0.5))
-      ctx.stroke() 
-
-
-      // X coord
-      ctx.beginPath()
-      ctx.moveTo(this.xCoord(this.x0), this.yCoord(this.y0))
-      ctx.lineTo(this.xCoord(this.xMax), this.yCoord(this.y0))
-      // -X coord
-      ctx.moveTo(this.xCoord(this.x0), this.yCoord(this.y0))
-      ctx.lineTo(this.xCoord(this.xMin), this.yCoord(this.y0))
-      ctx.stroke() 
-      
-      // X Divisions
-      ctx.beginPath()
-      for (let i = this.xDistance; i < this.xMax; i+=this.xDistance) {
-        ctx.moveTo(this.xCoord(i), this.yCoord(this.y0) - 4)
-        ctx.lineTo(this.xCoord(i), this.yCoord(this.y0) + 4)
-        ctx.fillText(i.toString(), this.xCoord(i), this.yCoord(this.y0) + 12)
-      }
-      for (let i = 0; i > this.xMin; i-=this.xDistance) {
-        ctx.moveTo(this.xCoord(i), this.yCoord(this.y0) - 4)
-        ctx.lineTo(this.xCoord(i), this.yCoord(this.y0) + 4)
-        ctx.fillText(i.toString(), this.xCoord(i), this.yCoord(this.y0) + 12)
-      }
-      ctx.stroke()
-
-      // Arrows (X)
-      ctx.beginPath()
-      ctx.moveTo(this.xCoord(this.xMax), this.yCoord(this.y0))
-      ctx.lineTo(this.xCoord(this.xMax-0.5), this.yCoord(this.y0-0.5))
-      ctx.moveTo(this.xCoord(this.xMax), this.yCoord(this.y0))
-      ctx.lineTo(this.xCoord(this.xMax-0.5), this.yCoord(this.y0+0.5))
-      ctx.stroke()
-
-      
-      // Subdivitions
-      ctx.lineWidth = 1
-      ctx.beginPath()
-      ctx.strokeStyle = '#0008'
-      for (let i = 0; i < this.xMax; i += this.xDistance) {
-        ctx.moveTo(this.xCoord(i), this.yCoord(this.xMin))
-        ctx.lineTo(this.xCoord(i), this.yCoord(this.xMax))
-      }
-      for (let i = 0; i > this.xMin; i -= this.xDistance) {
-        ctx.moveTo(this.xCoord(i), this.yCoord(this.xMin))
-        ctx.lineTo(this.xCoord(i), this.yCoord(this.xMax))
-      }
-      for (let i = 0; i < yMax; i += this.yDistance) {
-        ctx.moveTo(this.xCoord(this.xMin), this.yCoord(i))
-        ctx.lineTo(this.xCoord(this.xMax), this.yCoord(i))
-      }
-      for (let i = 0; i > yMin; i -= this.yDistance) {
-        ctx.moveTo(this.xCoord(this.xMin), this.yCoord(i))
-        ctx.lineTo(this.xCoord(this.xMax), this.yCoord(i))
-      }
-      ctx.stroke()
+      GraphRenderer.setupCanvas()
     },
     getDimensions() {
-      this.width = getClientWindowWidth()
-      this.height = getClientWindowHeight()
+      this.width = GraphRenderer.getClientWindowWidth()
+      this.height = GraphRenderer.getClientWindowHeight()
     }
   },
   mounted(){
-    var graph = new Graph()
-    setupCanvas();
+    this.setupCanvas();
   }
 })
 app.mount('#app')
